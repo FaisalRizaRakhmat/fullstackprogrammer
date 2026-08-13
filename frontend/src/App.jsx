6e9c1cf47f5fa94d7ecd658f5f332c54715a426d
FaisalRizaRakhmat <40479255+FaisalRizaRakhmat@users.noreply.github.com>
2026-08-13 11:20:11 +0700

    Create Form Input Voucher Seat Management

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,111 +7,74 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+  const inputRef = useState(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(`Submitted Value: ${inputRef.current.value}`);
+  };
+
+  const [selectedValue, setSelectedValue] = useState("");
+
+  const handleChange = (event) => {
+    setSelectedValue(event.target.value);
+  };
+
+  const [selectedDate, setSelectedDate] = useState('');
+
+  const handleDateChange = (event) => {
+    setSelectedDate(event.target.value); // Value will be a "YYYY-MM-DD" string
+  };
+
+
   return (
     <>
       <section id="center">
-        <div className="hero">
+        {/* <div className="hero">
           <img src={heroImg} className="base" width="170" height="179" alt="" />
           <img src={reactLogo} className="framework" alt="React logo" />
           <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
+        </div> */}
         <div>
-          <h1>Get started</h1>
+          <h1>Form Voucher Seat Management</h1>
           <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
+            <form onSubmit={handleSubmit}>
+              <label htmlFor="crewName">Crew Name: </label>
+              <input id="crewName" type="text" ref={inputRef} placeholder="Input Crew Name" />
+              <br/>
+              <label htmlFor="crewId">Crew ID: </label>
+              <input id="crewId" type="text" ref={inputRef} placeholder="Input Crew Id" />
+              <br/>
+              <label htmlFor="flightNumber">Flight Number: </label>
+              <input id="flightNumber" type="text" ref={inputRef} placeholder="Input Flight Number" />
+              <br/>
+
+              <label htmlFor="flightDate">Select Flight Date: </label>
+              <input type="date" id="flightDate"
+                name="flightDate"
+                value={selectedDate}
+                onChange={handleDateChange}
+              />
+              <br/>
+
+              <label htmlFor="aircraftType">Aircraft Type: </label>
+              <select id="aircraftType" value={selectedValue} onChange={handleChange}>
+                <option value="" disabled>-- Please select Aircraft Type--</option>
+                <option value="atr">ATR</option>
+                <option value="airBus320">Air Bus 320</option>
+                <option value="boeing737Max">Boeing 737 Max</option>
+              </select>
+              <br/><br/>
+              <button type="submit" className="counter">Submit</button>
+            </form>
+
+          
           </p>
         </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
+        
       </section>
 
       <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
 
       <div className="ticks"></div>
       <section id="spacer"></section>
