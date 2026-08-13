7532ccf3856988b7ced475fcbdaa822f50f0035d
FaisalRizaRakhmat <40479255+FaisalRizaRakhmat@users.noreply.github.com>
2026-08-13 18:06:49 +0700

    fixing 'message': 'CSRF token mismatch.'

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -12,6 +12,9 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //
+        $middleware->validateCsrfTokens(except: [
+            '/api/check','/api/generate'
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
