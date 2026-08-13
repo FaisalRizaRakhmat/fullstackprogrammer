d19eb3901a51d8fd9d2d0019a20c24166645f857
FaisalRizaRakhmat <40479255+FaisalRizaRakhmat@users.noreply.github.com>
2026-08-13 11:57:40 +0700

    change route into POST Controller

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->away('https://google.com');
 });
+
+Route::post('/posts', [PostController::class, 'store']);
